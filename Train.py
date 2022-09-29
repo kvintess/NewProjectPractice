5e4fc1ee5fd3dbc1c39a09aac1de6d7dd4f4b76f
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-29 20:34:40 +0300

    раззиповали файл

--- Train.py
+++ Train.py
@@ -4,4 +4,6 @@ import zipfile
 
 zip_file_name = 'voyna-i-mir.txt.zip'
 zip_file = zipfile.ZipFile(zip_file_name, 'r')
-zip_file.printdir()
+for filename in zip_file.namelist():
+    zip_file.extract(filename)
+'''раззиповали файл и вытащили в директорию, но кодировка не ютф8'''
